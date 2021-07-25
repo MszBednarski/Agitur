99a5a0157fa21bbecc53ee5ca193b8d8e34c1d99
MszBednarski <msz.bednarski@gmail.com>
2021-07-25 13:05:28 +0100

    disp offers' :

--- frontend/components/BalanceDisp/index.tsx
+++ frontend/components/BalanceDisp/index.tsx
@@ -3,15 +3,17 @@ import { makeAutoObservable, runInAction } from "mobx";
 import { Box, Text } from "grommet";
 import { getNewServer } from "../../shared";
 import { useEffect, useMemo } from "react";
-import { AssetResponse } from "./interfaces";
+import { AssetResponse, OfferResponse, SomeAsset } from "./interfaces";
+import * as StellarSdk from "stellar-sdk";
 
 class BalanceDispState {
   assetBalances: AssetResponse[] = [];
+  offers: OfferResponse[] = [];
   constructor() {
     makeAutoObservable(this);
   }
-  getBalances(pubkey: string) {
-    return getNewServer()
+  getDisp(pubkey: string) {
+    const unmount1 = getNewServer()
       .accounts()
       .accountId(pubkey)
       .cursor("now")
@@ -26,12 +28,48 @@ class BalanceDispState {
           });
         },
       });
+    const handleOffer = async (
+      page: StellarSdk.ServerApi.CollectionPage<StellarSdk.ServerApi.OfferRecord>
+    ) => {
+      const offers = page.records as OfferResponse[];
+      runInAction(() => {
+        this.offers = offers;
+      });
+    };
+    getNewServer()
+      .offers()
+      .forAccount(pubkey)
+      .call()
+      .then((page) => handleOffer(page));
+    return () => {
+      unmount1();
+    };
+  }
+
+  private getAssetString(asset: OfferResponse["buying"]) {
+    if (asset.asset_type == "native") {
+      return "XLM";
+    }
+    const someAsset = asset as SomeAsset;
+    return `${someAsset.asset_code}`;
   }
   get disp() {
     return this.assetBalances.map((asset) => (
       <Box key={`assetdisp${asset.asset_code}${asset.asset_issuer}`}>
         <Text>{`Asset issuer: ${asset.asset_issuer}`}</Text>
         <Text>{`${asset.asset_code}: ${asset.balance}`}</Text>
+        {this.offers.length != 0 && (
+          <>
+            <Text>{`Market:`}</Text>
+            {this.offers.map((offer) => (
+              <Text key={`offr${offer.id}`}>{`${this.getAssetString(
+                offer.buying
+              )} -> ${this.getAssetString(offer.selling)} Price: ${
+                offer.price
+              } Amount: ${offer.amount}`}</Text>
+            ))}
+          </>
+        )}
       </Box>
     ));
   }
@@ -43,7 +81,7 @@ class BalanceDispState {
 export const BalanceDisp = observer(({ pubkey }: { pubkey: string }) => {
   const state = useMemo(() => new BalanceDispState(), []);
   useEffect(() => {
-    return state.getBalances(pubkey);
+    return state.getDisp(pubkey);
   }, [pubkey, state]);
-  return <Box>{state.disp}</Box>;
+  return <Box flex="grow">{state.disp}</Box>;
 });
--- frontend/components/BalanceDisp/interfaces.ts
+++ frontend/components/BalanceDisp/interfaces.ts
@@ -10,3 +10,26 @@ export interface AssetResponse {
   asset_code: string;
   asset_issuer: string;
 }
+
+interface Native {
+  asset_type: "native";
+}
+
+export interface SomeAsset {
+  asset_type: string;
+  asset_code: string;
+  asset_issuer: string;
+}
+
+export interface OfferResponse {
+  amount: string;
+  buying: SomeAsset | Native;
+  id: string;
+  last_modified_ledger: number;
+  last_modified_time: string;
+  paging_token: string;
+  price: string;
+  price_r: { n: number; d: number };
+  seller: string;
+  selling: SomeAsset | Native;
+}
